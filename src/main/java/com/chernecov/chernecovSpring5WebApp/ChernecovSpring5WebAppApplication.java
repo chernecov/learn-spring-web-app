456beb1fa45cce2b03f2fe1d2577cf10ebbd6f3d
Sergey Chernecov <sergey.chernecov@gmail.com>
2018-04-01 16:42:57 +0300

    Initial implementation of simple web app

---
+++ src/main/java/com/chernecov/chernecovSpring5WebApp/ChernecovSpring5WebAppApplication.java
@@ -0,0 +1,15 @@
+package com.chernecov.chernecovSpring5WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChernecovSpring5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+				ChernecovSpring5WebAppApplication.class,
+				args
+		);
+	}
+}
